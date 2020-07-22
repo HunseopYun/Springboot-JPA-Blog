2497e2d1947ea907ac8c2bddc8fbd90b0d36db7f
HunseopYun <hunseop.yun@gmail.com>
2020-07-22 18:31:56 +0900

    스프링부트 강좌 39강(블로그 프로젝트) - 회원가입 하기 두번째 완료

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,16 +1,30 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
+
+
+
 
 //html파일이 아니라 data를 리턴해주는 controller = RestController
 @RestController
@@ -18,6 +32,63 @@ public class DummyControllerTest {
 	
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	
+	// save함수는 id를 전달하지 않으면 insert를 해주고
+	// save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+	// save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 함
+	// email, password
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			return "삭제에 실패하였습니다. 해당 id는 DB에 없습니다.";
+		}
+	
+		
+		return "삭제되었습니다. id : " + id;
+	}
+	
+	@Transactional // 함수 종료시에 자동 commit이 됨
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { // Json 데이터를 요청 -> Java Object(MessageConverter의 Jackson 라이브러리가 로 변환해서 받아줌)
+		System.out.println("id : " + id);
+		System.out.println("password : " + requestUser.getPassword());
+		System.out.println("email : "  + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+		return new IllegalArgumentException("수정에 실패하였습니다.");	
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		//userRepository.save(user);
+		//트랙잭션 어노테이션을 걸면 세이브하지 않아도 값이 자동으로 변경된다.
+		//더티 체킹
+		return user;
+	}
+	
+	
+	
+	// http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	// 한페이지당 2건에 데이터를 리턴받아 볼 예정
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+	
+	
+	
 	
 	//{id} 주소로 파라미터를 전달 받을 수 있음
 	// http://localhost:8000/blog/dummy/user/5
@@ -37,8 +108,7 @@ public class DummyControllerTest {
 		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
 			@Override
 			public IllegalArgumentException get() {
-				// TODO Auto-generated method stub
-				return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+				return new IllegalArgumentException("해당 사용자가 없습니다.");
 			}
 		});
 		// 요청: 웹브라우저
---
+++ src/main/webapp/WEB-INF/views/layout/footer.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+</br>
+<div class="jumbotron text-center" style="margin-bottom: 0">
+	<p>Created by Me</p>
+	<p>080-2344-2348</p>
+	<p>부산 수영구 xx동</p>
+</div>
+
+
+
+</body>
+</html>
